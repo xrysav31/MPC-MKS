0005a5caa8c44cc2e1fe74e0248973a82d2456f0
Ondrej Rysavy <ondraryseznam.cz>
2023-09-21 11:56:51 +0200

    cv01

--- cv01/Core/Src/main.c
+++ cv01/Core/Src/main.c
@@ -96,10 +96,16 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	  LL_GPIO_SetOutputPin(LD2_GPIO_Port, LD2_Pin);
-	  LL_mDelay(200);
-	  GPIOA->BRR = (1<<5); //LL_GPIO_ResetOutputPin(LD2_GPIO_Port, LD2_Pin);
-	  LL_mDelay(200);
+	  static const uint8_t pole[] = {1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0,};
+	  for (uint8_t i = 0; i < sizeof(pole); i++) {
+		  if (pole[i] == 1) {
+			  LL_GPIO_SetOutputPin(LD2_GPIO_Port, LD2_Pin);
+		  } else {
+			  LL_GPIO_ResetOutputPin(LD2_GPIO_Port, LD2_Pin);
+		  }
+	      LL_mDelay(200);
+	  }
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
