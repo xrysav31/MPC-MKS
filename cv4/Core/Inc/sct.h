bda2f1bb93c78bccacf9c6bf9d9dd8abd5e20e9e
Ondrej Rysavy <ondraryseznam.cz>
2023-10-19 11:56:41 +0200

    Done

---
+++ cv4/Core/Inc/sct.h
@@ -0,0 +1,15 @@
+/*
+ * sct.h
+ *
+ *  Created on: Oct 12, 2023
+ *      Author: xrysav31
+ */
+
+#ifndef INC_SCT_H_
+#define INC_SCT_H_
+
+void sct_init(void);
+void sct_led(uint32_t value);
+void sct_value(uint16_t value,uint8_t led);
+
+#endif /* INC_SCT_H_ */
